65bf2185a29d8094ac4db62f846049e77096d215
kumiori <a.leon.baldelli@gmail.com>
2024-08-12 16:16:33 +0300

    building

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='philoui',
     version='0.1',
     packages=find_packages(),
+     include_package_data=True,
     install_requires=[
         # List dependencies here
         'streamlit',
